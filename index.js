f6a98f05613387841cacb9990ab1dcce3cb5e2ad
Jagdale <raj.jagdale@accenture.com>
2024-11-06 17:45:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+console.log(process.env);
+
+const express = require('express'); 
+const app = express(); 
+
+// Mention port on which listening
+const port = 3000;
+
+app.get('/', (req, res) => {
+    console.log('juicy p');
+    res.send('Welll cummm to port!!'); 
+});
+
+app.get('/bb', (req, res) => {
+    res.send('<h1>Oooo Meri ZOHRAJABEEN, ISHQ DA JAM BEECHA JA</h1>')
+});
+
+
+// Server is always listening. 
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+})
